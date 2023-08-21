f707c6301c4d48ba744ec82052aca14280eaf527
alexeykaratay <karatayalexey@gmail.com>
2023-08-21 21:26:17 +0300

    add inputEmailLocator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,4 +1,5 @@
 public class HomePage {
     String buttonEnterLocator = "//div[@class='auth-bar__item auth-bar__item--text']";
+    String inputEmailLocator = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
 
 }
