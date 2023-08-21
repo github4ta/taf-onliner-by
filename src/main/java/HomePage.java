edec71c482abe3996e86a9953ac1fe0abc4cc23f
Sergei Tsarik <serge.tsarik@gmail.com>
2023-08-21 21:27:28 +0300

    refactor moved locator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,5 +1,3 @@
 public class HomePage {
     String buttonEnterLocator = "//div[@class='auth-bar__item auth-bar__item--text']";
-    String inputEmailLocator = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
-
 }
--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,2 +1,3 @@
 public class LoginFormPage {
+    String inputEmailLocator = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
 }
