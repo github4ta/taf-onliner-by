e041e45b33a28756f61240d55e5b0afe6750be3b
Mihail Karizno <karisno@gmail.com>
2023-08-25 22:58:23 +0300

    PRE007 - Home page локаторы

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,2 +1,4 @@
 public class HomePage {
+    private String loginButton = "//div[@class='auth-bar__item auth-bar__item--text']";
+    private String copyrightText = "//div[@class='footer-style__copy']";
 }
