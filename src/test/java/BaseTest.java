a731cc9d670c26e2ba362b8394621f28d6cb1d2d
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-08-28 13:03:22 +0300

    some changes2 in class_ structures in new adstract class BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public abstract class BaseTest {
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
     @BeforeEach
     public void driverSetup() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.onliner.by/");
     }
