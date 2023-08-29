7c37c31482a89b4599797229c0659c1bd6bb83ac
alexeykaratay <karatayalexey@gmail.com>
2023-08-29 21:10:49 +0300

    reformat testHomePageOpened()

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,18 +1,11 @@
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class HomeTest extends BaseTest {
     @DisplayName("Test: Open home page and get copywriter")
     @Test
     public void testHomePageOpened() {
-        WebElement copywriterElement = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='footer-style__copy']")));
-        String actualCopywriterText = copywriterElement.getText();
+        HomePage homePage = new HomePage(driver);
+        String actualCopywriterText = homePage.getCopyrightText();
         String extendsCopywriterText = "© 2001—2023 Onlíner";
         Assertions.assertEquals(extendsCopywriterText, actualCopywriterText);
     }
