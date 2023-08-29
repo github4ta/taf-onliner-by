a4992d8dea7e4ba460c0d545e9d938783ca31137
alexeykaratay <karatayalexey@gmail.com>
2023-08-29 20:49:24 +0300

    add reformat code

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -12,10 +12,9 @@ public class HomeTest extends BaseTest {
     public void testHomePageOpened() {
         WebElement copywriterElement = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='footer-style__copy']")));
-    String actualCopywriterText = copywriterElement.getText();
+        String actualCopywriterText = copywriterElement.getText();
         String extendsCopywriterText = "© 2001—2023 Onlíner";
         Assertions.assertEquals(extendsCopywriterText, actualCopywriterText);
     }
-
 }
 
