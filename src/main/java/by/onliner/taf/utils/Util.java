f4eca46f8300dfeaf9e5f69bb70b436cbcea21d2
Vladimir <golikvladimir@gmail.com>
2023-08-29 19:22:53 +0300

    refactor class Util add method generateNickname

--- src/main/java/by/onliner/taf/utils/Util.java
+++ src/main/java/by/onliner/taf/utils/Util.java
@@ -1,4 +1,22 @@
 package by.onliner.taf.utils;
 
 public class Util {
+    /**
+     * Метод generateNickname(int numberOfCharacters) возвращает произвольную кобинацию букв латинского алфавита
+     * (большие и малые) и цифр, например, asD23fD3s.
+     * @param numberOfCharacters Общее количество знаков в Nickname
+     * @return Cгенерированный Nickname
+     */
+    public static String generateNickname(int numberOfCharacters) {
+        String name = "";
+        char ch;
+
+        for (int i = 0; i < numberOfCharacters; i++) {
+            do {
+                ch = (char) (Math.random() * 62 + 60);
+            } while ((57 < ch && ch < 65) || (90 < ch && ch < 97));
+            name = name + ch;
+        }
+        return name;
+    }
 }
