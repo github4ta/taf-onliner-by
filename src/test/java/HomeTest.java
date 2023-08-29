4c7be890a17db01ffbaf10456ed2ca389b89387f
alexeykaratay <karatayalexey@gmail.com>
2023-08-29 20:32:09 +0300

    add testHomePageOpened() in HomeTest

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,3 +1,21 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class HomeTest extends BaseTest {
+    @DisplayName("Test: Open home page and get copywriter")
+    @Test
+    public void testHomePageOpened() {
+        WebElement copywriterElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='footer-style__copy']")));
+    String actualCopywriterText = copywriterElement.getText();
+        String extendsCopywriterText = "© 2001—2023 Onlíner";
+        Assertions.assertEquals(extendsCopywriterText, actualCopywriterText);
+    }
+
 }
 
