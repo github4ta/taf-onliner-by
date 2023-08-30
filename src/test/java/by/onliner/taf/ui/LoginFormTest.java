7127d83d93d74a645dcd5f3add1f3bb93cfdc3ab
Olga Bernat <olga.alexenok@gmail.com>
2023-08-30 18:53:02 +0200

    added new method to the LoginFormTest class

--- src/test/java/by/onliner/taf/ui/LoginFormTest.java
+++ src/test/java/by/onliner/taf/ui/LoginFormTest.java
@@ -1,4 +1,27 @@
 package by.onliner.taf.ui;
 
+import by.onliner.taf.po.HomePage;
+import by.onliner.taf.po.LoginForm;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class LoginFormTest extends BaseTest {
+
+    @DisplayName("UI0004 - Login form: blank nickname and any password")
+    @Test
+    public void testLoginWithEmptyNickAndAnyPassword() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickLoginButton();
+        LoginForm loginForm = new LoginForm(driver);
+        loginForm.fillingNicknameInputWith("");
+        loginForm.fillingPasswordInputWith("123ABCabc");
+        loginForm.clickLoginButton();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String actualErrorMessage = loginForm.getErrorMessageEmptyNickAndAnyPassword();
+        String expectedErrorMessage = "Укажите ник или e-mail";
+        Assertions.assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
 }
