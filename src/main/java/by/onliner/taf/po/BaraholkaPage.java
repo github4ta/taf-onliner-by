303a22bbc7bcb21caf08da09781122293968c05c
Vladimir <golikvladimir@gmail.com>
2023-08-31 23:07:04 +0300

    add BaraholkaPage() constructor

--- src/main/java/by/onliner/taf/po/BaraholkaPage.java
+++ src/main/java/by/onliner/taf/po/BaraholkaPage.java
@@ -1,13 +1,19 @@
 package by.onliner.taf.po;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BaraholkaPage {
+    private WebDriver driver;
     private By baraholkaFormHeadeer = By.xpath("//h1[text()='Барахолка']");
+
+    public BaraholkaPage(WebDriver driver) {
+        this.driver = driver;
+    }
 
     public String getFormHeaderText() {
         //Инициадизация вебэлемента с ожиданием проверки присутствия элемента на странице
