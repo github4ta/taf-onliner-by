3512fbd53dad93b743b7c453d01ea6d4b1fb4473
Vladimir <golikvladimir@gmail.com>
2023-08-31 23:02:31 +0300

    add getFormHeaderText()

--- src/main/java/by/onliner/taf/po/BaraholkaPage.java
+++ src/main/java/by/onliner/taf/po/BaraholkaPage.java
@@ -1,4 +1,19 @@
 package by.onliner.taf.po;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BaraholkaPage {
+    private By baraholkaFormHeadeer = By.xpath("//h1[text()='Барахолка']");
+
+    public String getFormHeaderText() {
+        //Инициадизация вебэлемента с ожиданием проверки присутствия элемента на странице
+        //https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/ui/ExpectedConditions.html#presenceOfElementLocated(org.openqa.selenium.By)
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(baraholkaFormHeadeer))
+                .getText();
+    }
 }
