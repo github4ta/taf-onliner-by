4311a4db68dfb8ac57fb19fbed48b554ec35feb0
Vladimir <golikvladimir@gmail.com>
2023-08-31 23:25:40 +0300

    add testaraholkaOpened method

--- src/test/java/by/onliner/taf/ui/BaraholkaTest.java
+++ src/test/java/by/onliner/taf/ui/BaraholkaTest.java
@@ -1,5 +1,24 @@
 package by.onliner.taf.ui;
 
-public class BaraholkaTest extends BaseTest{
+import by.onliner.taf.po.BaraholkaPage;
+import by.onliner.taf.po.HomePage;
+import org.junit.jupiter.api.*;
 
+public class BaraholkaTest extends BaseTest {
+    @BeforeEach
+    void beforEachBaraholkaTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickBaraholkaLink();
+    }
+
+    @Test
+    @DisplayName("Проверка текста заголовка страницы Барахолка")
+    void testaraholkaOpened() {
+        Assertions.assertEquals("Барахолка", new BaraholkaPage(driver).getFormHeaderText());
+    }
+
+    @AfterEach
+    void afterEachBaraholkaTest() {
+        driver.quit();
+    }
 }
