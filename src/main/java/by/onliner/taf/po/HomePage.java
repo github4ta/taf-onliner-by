a9d97ff6732136c5790a9e544e4882c25a457dcb
Vladimir <golikvladimir@gmail.com>
2023-08-31 22:33:35 +0300

    add clickBaraholkaLink()

--- src/main/java/by/onliner/taf/po/HomePage.java
+++ src/main/java/by/onliner/taf/po/HomePage.java
@@ -3,11 +3,16 @@ package by.onliner.taf.po;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
     private String loginButton = "//div[@class='auth-bar__item auth-bar__item--text']";
     private String copyright = "//div[@class='footer-style__copy']";
+    private String baraholkaLink = "//a[@href='https://baraholka.onliner.by/']0";
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -16,6 +21,14 @@ public class HomePage {
     public void clickLoginButton() {
         WebElement loginButtonElement = driver.findElement(By.xpath(loginButton));
         loginButtonElement.click();
+    }
+
+    public void  clickBaraholkaLink() {
+        //Инициадизация вебэлемента с ожиданием проверки присутствия элемента на странице
+        //https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/ui/ExpectedConditions.html#presenceOfElementLocated(org.openqa.selenium.By)
+        WebElement baraholkaLinkElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(baraholkaLink)));
+        baraholkaLinkElement.click();
     }
 
     public String getCopyright() {
