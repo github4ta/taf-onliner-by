3da6a9e44289030799ba355b5222f0a3056535b5
alexeykaratay <karatayalexey@gmail.com>
2023-09-01 01:53:03 +0300

    add carMarketHeader Locator in ABPage

--- src/main/java/by/onliner/taf/po/ABPage.java
+++ src/main/java/by/onliner/taf/po/ABPage.java
@@ -1,4 +1,12 @@
 package by.onliner.taf.po;
 
+import org.openqa.selenium.WebDriver;
+
 public class ABPage {
+    private WebDriver driver;
+    private String carMarketHeader = "//h1[@class='vehicle-form__title vehicle-form__title_big-alter']";
+
+    public ABPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
