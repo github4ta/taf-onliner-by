b952a94b0fc5c5c71a9bb5e26ff4ef9733089032
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-09-01 21:29:09 +0300

    add saleLink and rentLink locators and getTextSaleLink(), getTextRentLink() to RealtyPage class

--- src/main/java/by/onliner/taf/po/RealtyPage.java
+++ src/main/java/by/onliner/taf/po/RealtyPage.java
@@ -1,4 +1,26 @@
 package by.onliner.taf.po;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.layertree.model.StickyPositionConstraint;
+
 public class RealtyPage {
+
+    private WebDriver driver;
+    private String saleLink = "//a[@href='https://r.onliner.by/pk'][text()='Продажа']";
+    private String rentLink = "//a[@href='https://r.onliner.by/ak'][text()='Аренда']";
+
+    public RealtyPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public String getTextSaleLink(){
+        WebElement textSaleLinkElement = driver.findElement(By.xpath(saleLink));
+        return textSaleLinkElement.getText();
+    }
+    public String getTextRentLink(){
+        WebElement textRentLink = driver.findElement(By.xpath(rentLink));
+        return textRentLink.getText();
+    }
 }
+
