0e52e80c7285378e43c24bebaf2625ed2aaffae9
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-09-01 22:53:49 +0300

    resolve conflict in homepage class

--- src/main/java/by/onliner/taf/po/HomePage.java
+++ src/main/java/by/onliner/taf/po/HomePage.java
@@ -3,6 +3,9 @@ package by.onliner.taf.po;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
