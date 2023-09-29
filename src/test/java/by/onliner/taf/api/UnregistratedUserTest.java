47e9dafab1131042c13f187e1fa0f5b9aa58097e
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-09-29 14:12:35 +0300

    add api test package refactor pom file add UnregistratedUserTest

---
+++ src/test/java/by/onliner/taf/api/UnregistratedUserTest.java
@@ -0,0 +1,23 @@
+package by.onliner.taf.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UnregistratedUserTest {
+    @DisplayName("Check login by unregistered user")
+    @Test
+    public void loginWithNonExistingCredentials() {
+        String requestBody = "{\n" +
+                "    \"login\": \"test@test.com\",\n" +
+                "    \"password\": \"1q2w3e4r5t\"\n" +
+                "}";
+        given().body(requestBody).header("Content-Type", "application/json")
+                .when().post("https://www.onliner.by/sdapi/user.api/login")
+                .then().statusCode(400)
+                .assertThat().body("errors.key[0]", equalTo("invalid_login_or_password"))
+                .body("errors[0].message", equalTo("Неверный логин или пароль"));
+    }
+}
