d3c62e7305004a8ff540d45f6b97755801a552ce
olgabernat <141057027+olgabernat@users.noreply.github.com>
2023-10-01 18:50:25 +0200

    API0006
    * add method - Проверка логина с пустыми данными
    
    * update testPostEmptyLoginAndPassword method

--- src/test/java/by/onliner/taf/api/LoginTest.java
+++ src/test/java/by/onliner/taf/api/LoginTest.java
@@ -34,4 +34,19 @@ public class LoginTest {
                 body("message", equalTo("Validation failed")).
                 body("errors.password[0]", equalTo("Укажите пароль"));
     }
+
+    @DisplayName("Check login with empty data")
+    @Test
+    public void testPostEmptyLoginAndPassword() {
+        String body = "{\n" +
+                "   \"login\": \"\",\n" +
+                "   \"password\": \"\"\n" +
+                "}";
+        given().header("Content-Type", "application/json").body(body).
+                when().post("https://www.onliner.by/sdapi/user.api/login")
+                .then().assertThat().statusCode(422)
+                .body("message", equalTo("Validation failed"))
+                .body("errors.login[0]", equalTo("Укажите ник или e-mail"))
+                .body("errors.password[0]", equalTo("Укажите пароль"));
+    }
 }
