300b6ed7cdf75f47d3800dd51bcdad84df1538a4
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-10-01 17:02:48 +0300

    refactor rename test class v.2.0

--- src/test/java/by/onliner/taf/api/LoginTest.java
+++ src/test/java/by/onliner/taf/api/LoginTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class loginTest {
+public class LoginTest {
     @DisplayName("Check login by unregistered user")
     @Test
     public void loginWithNonExistingCredentials() {
