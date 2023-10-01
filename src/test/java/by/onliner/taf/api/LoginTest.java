cb0e9a07846f8e1a1f766cdeb581d06f1b3e203b
alexeykaratay <karatayalexey@gmail.com>
2023-10-01 20:30:38 +0300

    add Test method testWithEmptyUserName() in LoginTest.java

--- src/test/java/by/onliner/taf/api/LoginTest.java
+++ src/test/java/by/onliner/taf/api/LoginTest.java
@@ -49,4 +49,18 @@ public class LoginTest {
                 .body("errors.login[0]", equalTo("Укажите ник или e-mail"))
                 .body("errors.password[0]", equalTo("Укажите пароль"));
     }
+
+    @DisplayName("Check login with empty user name")
+    @Test
+    public void testWithEmptyUserName(){
+        String body = "{\n" +
+                "    \"login\": \"\",\n" +
+                "    \"password\": \"1q2w3e4r5t\"\n" +
+                "}";
+        given().header("Content-Type","application/json").body(body).
+                when().post("https://www.onliner.by/sdapi/user.api/login")
+                .then().assertThat().statusCode(422)
+                .body("message", equalTo("Validation failed"))
+                .body("errors.login[0]",equalTo("Укажите ник или e-mail"));
+    }
 }
