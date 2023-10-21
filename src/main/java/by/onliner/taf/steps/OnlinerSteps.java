f41deafb753907d012d37fb1bafd249f33dd1a60
Sergei Tsarik <serge.tsarik@gmail.com>
2023-10-21 11:16:34 +0300

    init commit for refactor

--- src/main/java/by/onliner/taf/steps/OnlinerSteps.java
+++ src/main/java/by/onliner/taf/steps/OnlinerSteps.java
@@ -1,6 +1,12 @@
 package by.onliner.taf.steps;
 
+import by.onliner.taf.po.HomePage;
+import io.cucumber.java.en.Given;
+
 public class OnlinerSteps {
 
+    @Given("User is on the HomePage")
+    public void userIsOnTheHomePage() {
 
+    }
 }
